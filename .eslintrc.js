dab645ac1094bb0b4777a009b3ed2dfb2d0fefa0
latusdenis <deniskochka@gmail.com>
2019-07-19 23:49:35 +0300

    chore: Add Provider, edit eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: ['airbnb', 'prettier'],
   plugins: ['react', 'jsx-a11y', 'import'],
   rules: {
-    'react/jsx-filename-extension': 0
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'global-require': 0
   }
 };
--- App.js
+++ App.js
@@ -1,45 +1,22 @@
-import { AppLoading } from "expo";
-import { Asset } from "expo-asset";
-import * as Font from "expo-font";
-import React, { useState } from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-
-import AppNavigator from "./src/AppNavigator";
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
-  }
-}
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
+import { Platform, StatusBar } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
+import AppNavigator from './src/AppNavigator';
+import store from './src/store/store';
 
 async function loadResourcesAsync() {
   await Promise.all([
-    Asset.loadAsync([
-      require("./assets/images/robot-dev.png"),
-      require("./assets/images/robot-prod.png")
-    ]),
+    Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
       ...Ionicons.font,
       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
       // remove this if you are not using it in your app
-      "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
     })
   ]);
 }
@@ -54,9 +31,24 @@ function handleFinishLoading(setLoadingComplete) {
   setLoadingComplete(true);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
+const App = ({ skipLoadingScreen }) => {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  if (!isLoadingComplete && !skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
   }
-});
+  return (
+    <Provider store={store}>
+      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      <AppNavigator />
+    </Provider>
+  );
+};
+
+export default App;
